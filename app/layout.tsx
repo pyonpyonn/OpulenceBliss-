55ce5e282d35e6b09527c19bea84706097d17782
bocchisu <121247329+pyonpyonn@users.noreply.github.com>
2026-08-23 20:50:16 +0530

    Load homepage full bleed hero style

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./opulence-home.css";
 import "./opulence-family-hero.css";
 import "./service-art.css";
 import "./homepage-width.css";
+import "./homepage-hero-fullbleed.css";
 
 import TopBar from "@/components/TopBar";
 import SiteHeader from "@/components/SiteHeader";
